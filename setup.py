7bb02c3820c9f059acabc8ff04f5475b15d0664d
Skrynkovskyy Igor <skrynkovskyy@gmail.com>
2014-02-28 18:25:01 +0200

    remove

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 """
 Json to query url
-_________________
+-----------------
 
     echo '{ "title": "Бумажки: простая игра на сегодняшний вечер / Блог компании Мосигра / Хабрахабр", "url": "http://habrahabr.ru/company/mosigra/blog/207906/" }' | json2query | pbcopy
     echo '{ "post": { "title": "Бумажки: простая игра на сегодняшний вечер / Блог компании Мосигра / Хабрахабр", "url": "http://habrahabr.ru/company/mosigra/blog/207906/" }}' | json2query | pbcopy
